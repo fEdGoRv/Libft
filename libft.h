93f43d2e4558de6448ff8b1fdd4e99f6ef4c720e
Federico <vilatte612@gmail.com>
2023-01-22 16:32:25 +0100

    isascii

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fede <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:55:02 by fede              #+#    #+#             */
-/*   Updated: 2023/01/16 17:19:58 by fede             ###   ########.fr       */
+/*   Updated: 2023/01/20 11:17:06 by fede             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
